c3f0f645d189c47d2902fe623148d4702a15cdac
zhangmenyucom <zhangmenyucom@github.com>
2018-08-19 12:17:15 +0800

    更改数据库表名称

--- wx-mall/pages/pay/pay.js
+++ wx-mall/pages/pay/pay.js
@@ -10,8 +10,8 @@ Page({
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     this.setData({
-      orderId: options.orderId,
-      actualPrice: options.actualPrice
+      orderId: options.id,
+      actualPrice: options.actual_price
     })
   },
   onReady: function () {
