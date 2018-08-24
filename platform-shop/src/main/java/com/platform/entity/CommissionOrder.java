6dac4f410c0ed63c0ad2b4bb735a2562645be682
zhangmenyucom <516195940@qq.com>
2018-08-24 21:56:53 +0800

    更新

--- platform-shop/src/main/java/com/platform/entity/CommissionOrder.java
+++ platform-shop/src/main/java/com/platform/entity/CommissionOrder.java
@@ -2,23 +2,46 @@ package com.platform.entity;/**
  * ${author} on 2018/8/24.
  */
 
+import lombok.Data;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * @author zhangxiaolu
  * @描述
  * @since 2018/8/24 9:03
  */
-public class CommissionOrder {
+@Data
+public class CommissionOrder implements Serializable {
 
-    /**主键id**/
+    /**
+     * 主键id
+     **/
     private Long id;
-    /**订单id**/
+    /**
+     * 订单id
+     **/
     private Long orderId;
-    /**分成收入**/
+    /**
+     * 分成收入
+     **/
     private BigDecimal gainBalance;
-    /**收入来源人id**/
+    /**
+     * 收入来源人id
+     **/
     private BigDecimal sourceUid;
-    /**分成收入说明**/
+    /**
+     * 分成收入说明
+     **/
     private String detail;
+    /**
+     * 创建时间
+     **/
+    private Date createTime;
+    /**
+     * 更新时间
+     **/
+    private Date updateTime;
 }
