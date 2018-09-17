14ac92b48a02d1672bde55a1a1c18d7c2a439b4a
zhangmenyucom <516195940@qq.com>
2018-09-17 16:47:35 +0800

    更新12321312

--- platform-api/src/main/java/com/platform/api/ApiUserController.java
+++ platform-api/src/main/java/com/platform/api/ApiUserController.java
@@ -12,6 +12,7 @@ import com.platform.service.ApiSignRecordService;
 import com.platform.service.ApiUserService;
 import com.platform.util.ApiBaseAction;
 import com.platform.utils.AESDecodeUtils;
+import com.platform.utils.Base64;
 import com.platform.utils.CharUtil;
 import com.platform.utils.MSUtil;
 import io.swagger.annotations.Api;
@@ -124,7 +125,7 @@ public class ApiUserController extends ApiBaseAction {
             token = request.getParameter(LOGIN_TOKEN_KEY);
         }
         try {
-            EncryptedDataBean decrypt = AESDecodeUtils.decrypt(token.getBytes(), iv.getBytes(), encryptedData.getBytes());
+            EncryptedDataBean decrypt = AESDecodeUtils.decrypt(Base64.decode(token).getBytes(), Base64.decode(iv).getBytes(),Base64.decode(encryptedData).getBytes());
             userVo.setMobile(decrypt.getPhoneNumber());
             userService.update(userVo);
             return toResponsObject(0, "手机绑定成功", decrypt);
