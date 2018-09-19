a1b9227e5176f1402466b24cda9c3ac61443880c
zhangmenyucom <516195940@qq.com>
2018-09-19 19:18:45 +0800

    更新12321312

---
+++ platform-common/src/main/java/com/platform/service/SysRegionService.java
@@ -0,0 +1,15 @@
+package com.platform.service;
+
+
+import com.platform.entity.SysRegionEntity;
+
+/**
+ * Service接口
+ *
+ * @author lipengjun
+ * @email 939961241@qq.com
+ * @date 2017-11-04 11:19:31
+ */
+public interface SysRegionService extends  BaseService<SysRegionEntity> {
+
+}
