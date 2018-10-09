f81600342638f8b66b5734359c02ec07b72baf1a
zhangmenyucom <516195940@qq.com>
2018-10-09 16:09:31 +0800

    更新12321312

--- platform-shop/src/main/java/com/platform/service/impl/GiftServiceImpl.java
+++ platform-shop/src/main/java/com/platform/service/impl/GiftServiceImpl.java
@@ -17,42 +17,6 @@ import java.util.Map;
  * @date 2018-09-12 18:50:18
  */
 @Service("giftService")
-public class GiftServiceImpl implements GiftService {
-    @Autowired
-    private GiftDao giftDao;
+public class GiftServiceImpl extends BaseServiceImpl<GiftEntity,GiftDao> implements GiftService {
 
-    @Override
-    public GiftEntity queryObject(Long id) {
-        return giftDao.queryObject(id);
-    }
-
-    @Override
-    public List<GiftEntity> queryList(Map<String, Object> map) {
-        return giftDao.queryList(map);
-    }
-
-    @Override
-    public int queryTotal(Map<String, Object> map) {
-        return giftDao.queryTotal(map);
-    }
-
-    @Override
-    public int save(GiftEntity gift) {
-        return giftDao.save(gift);
-    }
-
-    @Override
-    public int update(GiftEntity gift) {
-        return giftDao.update(gift);
-    }
-
-    @Override
-    public int delete(Long id) {
-        return giftDao.delete(id);
-    }
-
-    @Override
-    public int deleteBatch(Long[] ids) {
-        return giftDao.deleteBatch(ids);
-    }
 }
