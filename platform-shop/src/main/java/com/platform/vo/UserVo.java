376e1dbce55fdf2fd876d2391781f837ebfcc669
zhangmenyucom <zhangmenyucom@github.com>
2018-10-28 21:12:59 +0800

    haha

--- platform-shop/src/main/java/com/platform/vo/UserVo.java
+++ platform-shop/src/main/java/com/platform/vo/UserVo.java
@@ -58,23 +58,4 @@ public class UserVo extends BaseEntity implements Serializable {
     private Long point;
     /**下级列表**/
     private List<UserVo> subUserList;
-    /**总佣金**/
-    private BigDecimal totalBalance;
-    /**可用佣金**/
-    private BigDecimal avilableBalance;
-    /**冻结佣金**/
-    private BigDecimal lockBalance;
-
-    public static void main(String[] args) {
-        CommissionOrderVo commission=new CommissionOrderVo();
-        commission.setGainBalance(BigDecimal.valueOf(5.4));
-        UserVo user=new UserVo();
-        user.setTotalBalance(BigDecimal.valueOf(1.2));
-        user.setAvilableBalance(BigDecimal.valueOf(1.3));
-        UserVo grandP = new UserVo();
-        grandP.setUserId(user.getUserId());
-        grandP.setTotalBalance(user.getTotalBalance().add(new BigDecimal(""+commission.getGainBalance())));
-        grandP.setAvilableBalance(user.getAvilableBalance().add(new BigDecimal(""+commission.getGainBalance())));
-        System.out.println(JsonUtil.getJsonByObj(grandP));
-    }
 }
