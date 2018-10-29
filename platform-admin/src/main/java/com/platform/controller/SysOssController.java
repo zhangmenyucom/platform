a73a930d2c9842191a31e14c626797786e9db12c
zhangmenyucom <516195940@qq.com>
2018-10-29 11:11:44 +0800

    更新12321312

--- platform-admin/src/main/java/com/platform/controller/SysOssController.java
+++ platform-admin/src/main/java/com/platform/controller/SysOssController.java
@@ -14,10 +14,8 @@ import com.platform.validator.group.QcloudGroup;
 import com.platform.validator.group.QiniuGroup;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
@@ -32,7 +30,7 @@ import java.util.Map;
  * @email 516195940@qq.com
  * @date 2017-03-25 12:13:26
  */
-@RestController
+@Controller
 @RequestMapping("/sys/oss")
 public class SysOssController {
     @Autowired
@@ -48,6 +46,7 @@ public class SysOssController {
      * @param params 请求参数
      * @return R
      */
+    @ResponseBody
     @RequestMapping("/list")
     @RequiresPermissions("sys:oss:all")
     public R list(@RequestParam Map<String, Object> params) {
@@ -67,12 +66,13 @@ public class SysOssController {
      *
      * @return R
      */
-    @RequestMapping("/com/platform/config")
+    @ResponseBody
+    @RequestMapping("/config")
     @RequiresPermissions("sys:oss:all")
     public R config() {
         CloudStorageConfig config = sysConfigService.getConfigObject(KEY, CloudStorageConfig.class);
 
-        return R.ok().put("com/platform/config", config);
+        return R.ok().put("config", config);
     }
 
 
@@ -82,6 +82,7 @@ public class SysOssController {
      * @param config 配置信息
      * @return R
      */
+    @ResponseBody
     @SysLog("保存云存储配置信息")
     @RequestMapping("/saveConfig")
     @RequiresPermissions("sys:oss:all")
@@ -112,6 +113,7 @@ public class SysOssController {
      * @return R
      * @throws Exception 异常
      */
+    @ResponseBody
     @RequestMapping("/upload")
     public R upload(@RequestParam("file") MultipartFile file) throws Exception {
         if (file.isEmpty()) {
@@ -140,6 +142,7 @@ public class SysOssController {
      * @return R
      */
     @SysLog("删除图片")
+    @ResponseBody
     @RequestMapping("/delete")
     @RequiresPermissions("sys:oss:all")
     public R delete(@RequestBody Long[] ids) {
@@ -155,6 +158,7 @@ public class SysOssController {
      * @return R
      */
     @RequestMapping("/queryAll")
+    @ResponseBody
     public List<String> queryAll(@RequestParam Map<String, Object> params) {
         /**查询列表数据**/
         List<SysOssEntity> sysOssList = sysOssService.queryList(params);
